a7339243b09badca2af179c832427092c6b8fc65
Warren McDonald <warrenjmac@mail.com>
2019-08-31 19:08:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,7 @@ end
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
-#   known_for: => "Ruby",
+#   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
