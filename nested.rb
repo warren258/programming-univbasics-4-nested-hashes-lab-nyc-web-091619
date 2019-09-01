44c3b28358336b7b3eee8d174ed6acf1b01146b1
Warren McDonald <warrenjmac@mail.com>
2019-09-01 04:00:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
 
     programmer_hash[:yukihiro_matsumoto] = {}
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    programmer_hash[:yukihiro_matsumoto][]
+    programmer_hash[:yukihiro_matsumoto][:languages] = []
 end
 
 def changing_alan
