9a8c4e65262e81dd710d95981afabc0d1de3b8d4
Warren McDonald <warrenjmac@mail.com>
2019-09-01 04:39:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,8 +110,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay] = 
-     
+     programmer_hash[:alan_kay] = alans_new_info
+     return 
 end
 
 def adding_to_dennis
